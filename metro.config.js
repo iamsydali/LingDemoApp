f54920d9e4a075fab2d913953c3b4ffb076eb414
iamsydali <iamsydali@gmail.com>
2024-06-06 04:32:07 +0500

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,26 @@
-const {getDefaultConfig} = require('metro-config');
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 
-module.exports = (async () => {
-  const {
-    resolver: {sourceExts, assetExts},
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter(ext => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-    },
-  };
-})();
+const defaultConfig = getDefaultConfig(__dirname);
+const {
+  resolver: {sourceExts, assetExts},
+} = defaultConfig;
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+const config = {
+  transformer: {
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    assetExts: assetExts.filter(ext => ext !== 'svg'),
+    sourceExts: [...sourceExts, 'svg'],
+  },
+  watchFolders: [path.resolve(__dirname, '../')],
+};
+
+module.exports = mergeConfig(defaultConfig, config);
